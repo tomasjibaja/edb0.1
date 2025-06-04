4898631cb4d3d4367dbb8850c4be634000aaab6d
Tomas Jibaja <tomasjibaja@gmail.com>
2025-06-03 21:05:37 -0300

    Agregada animación de imagen de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LoginForm from '../components/LoginForm'
 import './Login.css'
 
 const Login = () => {
+
+  const [ imageZoom, setImageZoom ] = useState('');
+
   return (
     <div className='login'>
-      <img src="./login.png" alt="login-img" className="login--img" />
+      <img src="./login.png" alt="login-img" className={`login--img ${imageZoom}`} />
+      <div className="login--zoom-grid">
+        <div onMouseEnter={() => setImageZoom('zoom-top-left')} onMouseLeave={() => setImageZoom('')}></div>
+        <div onMouseEnter={() => setImageZoom('zoom-top-right')} onMouseLeave={() => setImageZoom('')}></div>
+        <div onMouseEnter={() => setImageZoom('zoom-bottom-left')} onMouseLeave={() => setImageZoom('')}></div>
+        <div onMouseEnter={() => setImageZoom('zoom-bottom-right')} onMouseLeave={() => setImageZoom('')}></div>
+      </div>
       <LoginForm />
     </div>
   )

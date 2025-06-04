f06db1629bb399b226ad3791efb5dad70d1d0ed8
Tomas Jibaja <tomasjibaja@gmail.com>
2025-06-03 23:14:50 -0300

    Haciendo la pagina de cuenta

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react'
+import { useForm } from 'react-hook-form';
 import './Account.css'
 
 const Account = () => {
 
   const [ email, setEmail ] = useState(localStorage.getItem('email'));
+  const { register, formState: { errors }, handleSubmit } = useForm();
+
+  const onSubmit = () => {
+
+  }
 
   return (
     <div className='account'>
@@ -14,8 +20,25 @@ const Account = () => {
       </div>
       <div className="account--personal card">
         <h2>Datos personales</h2>
-        <h3>Correo electrónico:</h3>
-        <span>{email}</span>
+        <div className="account--personal-container">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor="user">Usuario</label>
+            <input placeholder='Nombre de usuario' {...register('user', { required: true })} />
+
+            <label htmlFor="email">Correo electrónico</label>
+            <input defaultValue={email} {...register('email', { required: true })} />
+
+            <a onClick={() => {}}>Cambiar contraseña</a>
+
+            <label htmlFor="dob">Fecha de nacimiento</label>
+            <input placeholder='DD/MM/AA' {...register('dob', { required: true })} />
+
+            <button type='submit'>Guardar cambios</button>
+          </form>
+          <div className="account--profile-pic">
+            <h3>Foto de perfil</h3>
+          </div>
+        </div>
       </div>
     </div>
   )
